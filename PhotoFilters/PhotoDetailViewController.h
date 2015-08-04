82907aafcd98112cf42bc4750b35fdf35a9830a6
Vincent Chau <vichau@ucdavis.edu>
2015-08-04 00:45:08 -0700

    detailviewcontroller

---
+++ PhotoFilters/PhotoDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  PhotoDetailViewController.h
+//  PhotoFilters
+//
+//  Created by Vincent Chau on 8/3/15.
+//  Copyright (c) 2015 Vincent Chau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PhotoDetailViewController : UIViewController
+
+@end
